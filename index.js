f2996f9d18b556da86a8ebac10ec58a135d4f544
daniellucasss <daniellucasss@live.com>
2017-12-23 12:08:40 -0200

    Fix: Correctingg a simple mistake

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Test from './components/test/test'
+
 ReactDOM.render(
-    <h1>Testing Parcel Bundler</h1>,
+    <div>
+        <h1>Testing Parcel Bundler</h1>
+        <Test />
+    </div>,
     document.getElementById('root')
 )
